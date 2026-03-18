b9d1418b4a882c5271cd208bcfa0b274446ac17d
platon.AI <galaxyeye@live.cn>
2026-03-18 17:27:34 +0800

    fix: update PrivateNetworkRequestPolicy to include WARN_FROM_INSECURE_TO_MORE_PRIVATE and add UNKNOWN fallback

--- cdt-kotlin-client/src/main/kotlin/ai/platon/cdt/kt/protocol/types/network/PrivateNetworkRequestPolicy.kt
+++ cdt-kotlin-client/src/main/kotlin/ai/platon/cdt/kt/protocol/types/network/PrivateNetworkRequestPolicy.kt
@@ -1,17 +1,18 @@
 @file:Suppress("unused")
 package ai.platon.cdt.kt.protocol.types.network
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue
 import com.fasterxml.jackson.`annotation`.JsonProperty
 
 public enum class PrivateNetworkRequestPolicy {
-  @JsonProperty("Allow")
-  ALLOW,
-  @JsonProperty("BlockFromInsecureToMorePrivate")
-  BLOCK_FROM_INSECURE_TO_MORE_PRIVATE,
-//  @JsonProperty("WarnFromInsecureToMorePrivate")
-//  WARN_FROM_INSECURE_TO_MORE_PRIVATE,
-
-  // Patch: vincent, 20251025, protocol upgraded. Or you can modify the received message before deserialization.
-  @JsonProperty("WarnFromInsecureToMorePrivate")
-  PERMISSION_BLOCK,
+    @JsonProperty("Allow")
+    ALLOW,
+    @JsonProperty("BlockFromInsecureToMorePrivate")
+    BLOCK_FROM_INSECURE_TO_MORE_PRIVATE,
+    @JsonProperty("WarnFromInsecureToMorePrivate")
+    WARN_FROM_INSECURE_TO_MORE_PRIVATE,
+    // patch by vincent: 20251028
+    // Default fallback for any unknown name
+    @JsonEnumDefaultValue
+    UNKNOWN,
 }

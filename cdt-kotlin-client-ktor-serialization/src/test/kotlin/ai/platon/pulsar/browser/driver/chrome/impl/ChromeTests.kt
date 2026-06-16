9b0129582f8fbbd1c5b2c09b8269029e48eb9ced
platon.AI <galaxyeye@live.cn>
2026-06-16 16:24:07 +0800

    test: add integration tests for ChromeLauncher and improve example usage

---
+++ cdt-kotlin-client-ktor-serialization/src/test/kotlin/ai/platon/pulsar/browser/driver/chrome/impl/ChromeTests.kt
@@ -0,0 +1,33 @@
+package ai.platon.pulsar.browser.driver.chrome.impl
+
+import ai.platon.pulsar.browser.driver.chrome.ChromeLauncher
+import ai.platon.pulsar.browser.driver.chrome.impl.EventDispatcher.Companion.JSON
+import ai.platon.pulsar.common.browser.BrowserFiles
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import kotlin.test.assertNotNull
+
+class ChromeTests {
+
+    val JSON = Json {
+        prettyPrint = true
+    }
+
+    @Test
+    fun chromeLaunch() {
+        val userDataDir = BrowserFiles.computeTestContextDir()
+        // val userDataDir = BrowserFiles.computeRandomTmpContextDir("test")
+        val launcher = ChromeLauncher(userDataDir)
+        val chrome = launcher.launch()
+        val version = chrome.version
+        println("version: " + JSON.encodeToString(version))
+        assertNotNull(version.browser)
+        assertNotNull(version.v8Version)
+
+        assertTrue(chrome.canConnect())
+        val tab = chrome.createTab("https://example.com/")
+        assertNotNull(tab.devtoolsFrontendUrl)
+        assertNotNull(tab.isPageType())
+    }
+}
--- cdt-kotlin-client-ktor-serialization/src/test/kotlin/ai/platon/pulsar/browser/driver/examples/LaunchExample.kt
+++ cdt-kotlin-client-ktor-serialization/src/test/kotlin/ai/platon/pulsar/browser/driver/examples/LaunchExample.kt
@@ -4,8 +4,8 @@ import ai.platon.pulsar.browser.driver.chrome.ChromeLauncher
 import ai.platon.pulsar.common.browser.BrowserFiles
 
 suspend fun main() {
-    // val userDataDir = BrowserFiles.computeTestContextDir()
-    val userDataDir = BrowserFiles.computeRandomTmpContextDir("test")
+    val userDataDir = BrowserFiles.computeTestContextDir()
+    // val userDataDir = BrowserFiles.computeRandomTmpContextDir("test")
     val launcher = ChromeLauncher(userDataDir)
     val chrome = launcher.launch()
     val version = chrome.version

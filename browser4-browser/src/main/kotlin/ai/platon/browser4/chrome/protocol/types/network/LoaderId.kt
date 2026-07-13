18ea642a8bcd693ff74a24af88569be43953503d
platon.AI <galaxyeye@live.cn>
2026-07-13 20:49:34 +0800

    feat: cdt-kotlin-client-serialization

--- browser4-browser/src/main/kotlin/ai/platon/browser4/chrome/protocol/types/network/LoaderId.kt
+++
@@ -1,6 +0,0 @@
-@file:Suppress("unused")
-package ai.platon.browser4.chrome.protocol.types.network
-
-import kotlin.String
-
-typealias LoaderId = String
--- browser4-browser/src/main/kotlin/ai/platon/browser4/chrome/protocol/types/network/TimeSinceEpoch.kt
+++
@@ -1,6 +0,0 @@
-@file:Suppress("unused")
-package ai.platon.browser4.chrome.protocol.types.network
-
-import kotlin.Double
-
-typealias TimeSinceEpoch = Double
